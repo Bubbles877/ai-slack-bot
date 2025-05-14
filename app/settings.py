7d58540ebe5ed9cd9dfc74be90e2e36b5e699de2
oha <banana-yogurt@ymail.ne.jp>
2025-05-14 23:08:16 +0900

    feat: 設定 (Settings) を追加

---
+++ app/settings.py
@@ -0,0 +1,20 @@
+from typing import Literal, Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """設定
+
+    環境変数から設定を読み込んで管理します。
+    """
+
+    log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR"] = "INFO"
+
+    port: Optional[int] = None
+
+    model_config = SettingsConfigDict(
+        extra="ignore",
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )

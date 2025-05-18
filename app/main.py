f784647968efc86faf4057eff40568b20a242f04
oha <banana-yogurt@ymail.ne.jp>
2025-05-18 18:26:57 +0900

    refactor: Main／Slack の処理を SlackBot 利用に差し替え

--- app/main.py
+++ app/main.py
@@ -3,45 +3,23 @@ import time
 
 import uvicorn
 from loguru import logger
-from slack_bolt.adapter.fastapi.async_handler import AsyncSlackRequestHandler
 from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
-from slack_bolt.async_app import AsyncApp
 
 from app.http_server import HTTPServer
 from app.settings import Settings
+from app.slack_bot import SlackBot
 from util.setting.slack_settings import SlackSettings
 
 settings = Settings()
 slack_settings = SlackSettings()
 
-app = AsyncApp(
-    token=slack_settings.bot_token, signing_secret=slack_settings.signing_secret
-)
-# app.client.token = os.getenv("SLACK_BOT_TOKEN")
-req_handler = AsyncSlackRequestHandler(app)
-
-
-@app.event("app_mention")
-# async def handle_app_mentions(body, say, logger):
-async def handle_app_mentions(body, say):
-    logger.info(body)
-    await say("What's up?")
-
-
-@app.event("message")
-async def handle_message(body, say):
-    logger.info(f"message: {body}")
-
-
-api = HTTPServer(
-    request_handler=req_handler,
-    enable_logging=settings.log_level == "DEBUG",
-)
+bot = SlackBot(settings=slack_settings, enable_logging=True)
+api = HTTPServer(request_handler=bot.request_handler(), enable_logging=True)
 
 
 async def _main() -> None:
     start_time = time.perf_counter()
-    handler = AsyncSocketModeHandler(app, slack_settings.app_token)
+    handler = AsyncSocketModeHandler(bot, slack_settings.app_token)
     await handler.start_async()
     await handler.close_async()
 
@@ -50,14 +28,10 @@ async def _main() -> None:
     )
 
 
-logger.info(f"{__name__=}")
-
 if __name__ == "__main__":
     try:
         if slack_settings.is_socket_mode:
-            logger.info("Socket mode is enabled")
-            # AsyncSocketModeHandler(app, slack_settings.app_token).start_async()
-            # asyncio.run(AsyncSocketModeHandler(app, slack_settings.app_token).start_async())
+            logger.info("Socket mode starting...")
             asyncio.run(_main())
         else:
             logger.info("HTTP server starting...")
@@ -66,7 +40,7 @@ if __name__ == "__main__":
                 app="main:api",
                 host="0.0.0.0",
                 port=settings.port if settings.port else 80,
-                reload=True,  # TODO: 本番環境では無効化
+                reload=settings.is_development,
             )
     except KeyboardInterrupt:
         logger.info("KeyboardInterrupt: Shutting down...")

3f4fae8b76f0ae9d70b3ac1645fcbb145051f832
oha <banana-yogurt@ymail.ne.jp>
2025-05-29 00:22:39 +0900

    feat: Main／LLM 関連の処理を仮組み

--- app/main.py
+++ app/main.py
@@ -8,20 +8,32 @@ from loguru import logger
 from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
 from slack_bolt.async_app import AsyncApp
 
+import util.llm_utils as llm_utils
 from app.http_server import HTTPServer
+from app.resource_loader import ResourceLoader
 from app.settings import Settings
 from app.slack_bot import SlackBot
+from util.llm_chat import LLMChat
+from util.setting.llm_settings import LLMSettings
 from util.setting.slack_settings import SlackSettings
 
 
 class Main:
-    def __init__(self, settings: Settings, slack_settings: SlackSettings):
+    def __init__(
+        self,
+        settings: Settings,
+        slack_settings: SlackSettings,
+        llm_settings: LLMSettings,
+    ):
         self._settings = settings
         self._slack_settings = slack_settings
+        self._llm_settings = llm_settings
 
         self._setup_logger(self._settings.log_level)
 
         logger.debug(f"Settings:\n{self._settings.model_dump_json(indent=2)}")
+
+        self._resource_loader = ResourceLoader(enable_logging=True)
 
         self._bot = SlackBot(settings=self._slack_settings, enable_logging=True)
         self._http_server: Optional[HTTPServer] = None
@@ -34,9 +46,23 @@ class Main:
                 enable_logging=True,
             )
 
+        llm_utils.enable_logging(enable=True)
+
+        if not (llm := llm_utils.create_llm(self._llm_settings)):
+            raise RuntimeError("Failed to create LLM")
+
+        self._llm_chat = LLMChat(
+            llm, self._settings.llm_max_messages, enable_logging=True
+        )
+
     async def setup(self) -> None:
         """セットアップする"""
         logger.debug("Setting up...")
+
+        llm_instructions = await self._resource_loader.load_plane_text(
+            self._settings.llm_instruction_file_path
+        )
+        self._llm_chat.configure(llm_instructions)
 
         await self._bot.setup()
 
@@ -124,6 +150,7 @@ logger.info(f"App main: {__name__=}")
 main: Optional[Main] = None
 settings = Settings()
 slack_settings = SlackSettings()
+llm_settings = LLMSettings()
 
 # if __name__ == "__main__":
 #     try:
@@ -136,10 +163,10 @@ slack_settings = SlackSettings()
 
 if __name__ == "__main__":
     if slack_settings.is_socket_mode:
-        main = Main(settings, slack_settings)
+        main = Main(settings, slack_settings, llm_settings)
         asyncio.run(_socket_mode_main(main, slack_settings.app_token))
     else:
         _http_server_main(settings)
 else:
     # Uvicorn や Gunicorn で実行する場合
-    main = Main(settings, slack_settings)
+    main = Main(settings, slack_settings, llm_settings)
